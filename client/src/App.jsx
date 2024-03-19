e3baebf656df982181adfcc1e9ec3ccdd91f64aa
brewchetta <chett.tiller@gmail.com>
2024-03-19 11:43:30 -0400

    init vite

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <h1>Cat Memes OMG</h1>
+
+      <p>To get started...</p>
+
+      <code>Let's add a proxy to our vite project.</code>
+    </>
+  )
+}
+
+export default App
